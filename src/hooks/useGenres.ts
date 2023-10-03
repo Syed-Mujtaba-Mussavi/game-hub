ca3e78606bb366ac4f50695a3d8112747b57ff27
mujtaba <mujtabasadat07800@gmail.com>
2023-10-04 01:01:44 +0430

    .

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,10 @@
-import genres from "../data/Genres";
+import Genres from "../data/genres";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null });
+const useGenres = () => ({ data: Genres, isLoading: false, error: null });
 
 export default useGenres;
